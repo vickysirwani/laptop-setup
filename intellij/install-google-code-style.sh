54f589d7509e2b9af2ee0f7fcacd430608e4bc9f
vickysirwani <vickysirwani@gmail.com>
2017-02-20 16:36:09 +0530

    Update install-google-code-style.sh

--- intellij/install-google-code-style.sh
+++ intellij/install-google-code-style.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 ideaulti=~/.IntelliJIdea*/config
 ideac=~/.IdeaIC*/config
-if [[ -d  $ideaulti ]]; then
+if [ -d  $ideaulti ]; then
   mkdir -p $ideaulti/codestyles
   wget -O $ideaulti/codestyles/google-style.xml https://raw.githubusercontent.com/google/styleguide/gh-pages/intellij-java-google-style.xml
 fi
 
-if [[ -d  $ideac ]]; then
+if [ -d  $ideac ]; then
   mkdir -p $ideac/codestyles
   wget -O $ideac/codestyles/google-style.xml https://raw.githubusercontent.com/google/styleguide/gh-pages/intellij-java-google-style.xml
 fi
